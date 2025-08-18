e5ada1ff1b2cd2d0469ea1f4ce7778a4ddf05be0
Ahmed Gacem <gacema426@gmail.com>
2025-08-18 13:38:18 -0400

    Added core files to project

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+const Webpack = require('webpack');
+
+module.exports = {
+    entry : {
+		index : './client/src/page-index.js',
+		meteo : './client/src/page-meteo.js',
+    },
+    mode : 'development',
+    cache: false,
+    output : {
+        filename : '[name].js',
+        path : path.resolve(__dirname, 'client/dist')
+    },
+    plugins: [
+    ]
+}
