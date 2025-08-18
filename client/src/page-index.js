24acc10bed81df3c5150bd96a0efebf5552c697b
Ahmed Gacem <gacema426@gmail.com>
2025-08-18 14:14:16 -0400

    Background image installed

--- client/src/page-index.js
+++ client/src/page-index.js
@@ -1,10 +1,19 @@
+import { showAlert } from "./utils";
+
 window.addEventListener("load", () => {
+
+    //showAlert();
+    
+    document.querySelector("#error-message").style.display = "none";
+
     document.querySelector("#password-form").onsubmit = () => {
         let success = true;
 
         if (document.querySelector("#password").value !== "web2") {
-            success = false;
+            
+            document.querySelector("#password").value = ""
             document.querySelector("#error-message").style.display = "block";
+            success = false;
         }
 
         return success;
