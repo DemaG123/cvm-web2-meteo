ee28ffadfe17b6096994ae9b28b5a6fe31b78eec
Ahmed Gacem <gacema426@gmail.com>
2025-08-18 18:43:19 -0400

    final commit of the day

--- client/src/js/seagull.js
+++ client/src/js/seagull.js
@@ -5,21 +5,24 @@ export class Seagull {
         this.node = document.getElementById(this.id);
         this.node.style.left = window.innerWidth + "px";
         this.node.style.top = Math.floor(Math.random() * 200) + "px";
-
+        this.node.addEventListener("mouseover", () => {
+            this.speedX = 15;
+        });
         this.speedX = 5;
 
     }
 
     
     tick(){
+        
         let newPositionY = this.node.offsetLeft - this.speedX;
 
         this.node.style.left = newPositionY + "px";
 
-        if (newPositionY == -175) {
+        if (newPositionY < -175) {
             this.node.style.left = window.innerWidth + "px";
             this.node.style.top = Math.floor(Math.random() * 200) + "px";
-            
+            this.speedX = 5;
         }
 
     }
