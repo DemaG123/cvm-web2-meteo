565fe0b10a5e6a140d311a9cc97de480e506d6a4
Ahmed Gacem <gacema426@gmail.com>
2025-08-19 21:22:23 -0400

    button for destination choice (unfinished)

--- client/src/page-meteo.js
+++ client/src/page-meteo.js
@@ -2,6 +2,16 @@ import { fetchData } from "./meteo-api";
 
 window.addEventListener("load", async () => {
     let weatherData = await fetchData(45.5019, 73.5674);
-    console.log(weatherData)
+    console.log(weatherData); 
+
+    // let btnDestination = document.getElementById("btn-destination");
+
+    const select = document.getElementById('island-select');
+    const button = document.getElementById('btn-destination');
+
+    button.onclick = (event) => {
+        const selectedIsland = select.value;
+        console.log("Selected island:", selectedIsland);
+    }
+
 })
-    
