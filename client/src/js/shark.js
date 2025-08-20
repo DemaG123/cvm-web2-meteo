b36c0f6824fcbf06a7c1b004975dc60f417b32b2
Ahmed Gacem <gacema426@gmail.com>
2025-08-20 10:36:00 -0400

    shark jumpscare added

--- client/src/js/shark.js
+++ client/src/js/shark.js
@@ -4,9 +4,26 @@ export class Shark {
         this.id = id;
         this.node = document.getElementById(this.id);
         this.node.style.left = window.innerWidth + "px";
-        this.node.style.top = Math.floor(Math.random() * 500) + "px";
+        this.node.style.top = Math.floor(Math.random() * 200) + "px";
         this.node.addEventListener("mouseover", () => {
-            this.speedX = 15;
+            this.node.style.backgroundImage = "url('img/shark-attack.gif')";
+            this.node.style.width = window.innerWidth + "px";
+            this.node.style.height = window.innerHeight + "px";
+            this.node.style.left = 0 + "px";
+            this.node.style.top = 0 + "px";
+            this.speedX = 0;    
+
+            setTimeout(() => {
+                this.node.style.backgroundImage = "url('img/shark.gif')";
+                this.speedX = 5;
+                this.node.style.left = window.innerWidth + "px";
+                this.node.style.top = Math.floor(Math.random() * 200) + "px";
+                this.node.style.width = 225 + "px";
+                this.node.style.height = 175 + "px";
+            }, 1000);
+
+
+
         });
         this.speedX = 5;        
     }
@@ -23,6 +40,5 @@ export class Shark {
             this.speedX = 5;
         }
 
-    }
-
+    }    
 }
