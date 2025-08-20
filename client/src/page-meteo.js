92b0e7e3e9b763464e4bcaf1d1c3597fc6411d69
Ahmed Gacem <gacema426@gmail.com>
2025-08-20 14:17:27 -0400

    visual added of the city chosen after 1 second

--- client/src/page-meteo.js
+++ client/src/page-meteo.js
@@ -1,34 +1,46 @@
 import { fetchData } from "./meteo-api";
 
 window.addEventListener("load", async () => {
-    let weatherData = await fetchData(45.5019, 73.5674);
-    console.log(weatherData); 
+    let weatherDataM = await fetchData(45.5019, -73.5674);
+    let weatherDataT = await fetchData(35.6762, 139.6503);
+    let weatherDataA = await fetchData(36.6997, 3.0588);
     
-    let listNode = document.querySelector("#info-list");
-
+    console.log(weatherDataM); 
+    
     const select = document.getElementById('island-select');
     const button = document.getElementById('btn-destination');
-
+    
+    let listNode = document.querySelector("#info-list");
+    
     button.onclick = (event) => {
-        const selectedIsland = select.value;
-        console.log("Selected island:", selectedIsland);
+    const selectedIsland = select.value;
         
-        // lignes 17, 18 et 19 -chatGPT
-        while (listNode.childNodes.length > 1) {
-        listNode.removeChild(listNode.lastChild);
-        }
-
         let newLineNode = document.createElement("div");
-        
+        let newImgNode = document.createElement("div");
+        newLineNode.style.margin = "10px auto 10px";
         newLineNode.innerText = selectedIsland;
-
-
+        listNode.innerHTML = "";
         
         listNode.append(newLineNode);
+        setInterval(() => {
+            
+            if (selectedIsland == "Montréal") {
+                newImgNode.style.backgroundImage = "url('img/montreal.avif')";
+            }
+            if (selectedIsland == "Tokyo") {
+                newImgNode.style.backgroundImage = "url('img/tokyo.jpg')";
+            }
+            if (selectedIsland == "Alger") {
+                newImgNode.style.backgroundImage = "url('img/alger.jpg')";
+            }
+            newImgNode.style.backgroundSize = "100% 100%" ;
+            newImgNode.style.width = "100%";
+            newImgNode.style.height = 300 + "px";
+            newImgNode.style.margin = "0 auto";
+            newImgNode.style.borderRadius = 20 + "px";
+        }, 1000);
+        listNode.append(newImgNode);
 
     }
-
-
-
 
 })
