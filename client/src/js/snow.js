baf1ee1f6d6e1c491d7772424c1e64b8bde282d4
Ahmed Gacem <gacema426@gmail.com>
2025-08-21 18:01:00 -0400

    upgrading the snow animation

--- client/src/js/snow.js
+++ client/src/js/snow.js
@@ -3,7 +3,7 @@ export class Snow {
     constructor(id) {
         this.id = id;
         this.node = document.getElementById(this.id);
-        this.node.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+        this.node.style.left = Math.floor(Math.random() * window.innerWidth * 1.5) + "px";
         this.node.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
         this.speedX = 5;
     }
@@ -11,11 +11,14 @@ export class Snow {
     tick(){
         
         let newPositionY = this.node.offsetTop + this.speedX;
+        let newPositionX = this.node.offsetLeft - 5;
 
         this.node.style.top = newPositionY + "px";
+        this.node.style.left = newPositionX + "px";
+
 
         if (newPositionY > window.innerHeight) {
-            this.node.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+            this.node.style.left = Math.floor(Math.random() * window.innerWidth * 1.5) + "px";
             this.node.style.top = -100 + "px";
         }
 
